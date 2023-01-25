43ae92cdb373ee8dec6fd3082c9e98bf72e331a7
Keerthana J <keerthanajothirameshkumar@gmail.com>
2023-01-25 18:54:05 +0530

    Class and object

---
+++ oops concept/oops concept/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class class1
+{
+    private int num1 = 50;
+    private int num2 = 100;
+    public void average()
+    {
+        int avg=(num1+ num2)/2;
+        Console.WriteLine("The average is "+avg);
+    }
+}
+class Program
+{
+    static void Main(String[] args)
+    {
+        class1 obj = new class1();
+        obj.average();
+        Console.WriteLine();
+    }
+}
