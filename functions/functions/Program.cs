c2bc45dc6a68f1272d9824d3a6c49250316f2e92
Keerthana J <keerthanajothirameshkumar@gmail.com>
2023-01-25 18:15:31 +0530

    Functions

---
+++ functions/functions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    public void display()
+    {
+        Console.WriteLine("This is an example code for functions in C#");
+    }
+    public void message()
+    {
+        Console.WriteLine("Non parameterized function!");
+    }
+    public static void Main(String[] args)
+    {
+        Program obj=new Program();
+        obj.display();
+        obj.message();
+    }
+}
