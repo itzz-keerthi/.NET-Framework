35dfd363681bf57b1272030b0d37433b04e5d98e
Keerthana J <keerthanajothirameshkumar@gmail.com>
2023-01-31 18:42:19 +0530

    Join operators

---
+++ join operators/join operators/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 25
+        Me.ListBox1.Location = New System.Drawing.Point(247, 115)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(281, 254)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(330, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Order details"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ join operators/join operators/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Customers = New List(Of Customer)(New Customer() {New Customer With {.Key = 1,
+                                            .Name = "Keerthi"}, New Customer With {.Key = 2,
+                                            .Name = "Jothi"}, New Customer With {.Key = 3,
+                                            .Name = "Banu"}, New Customer With {.Key = 4,
+                                            .Name = "Lathi"}, New Customer With {.Key = 5,
+                                            .Name = "Muthu"}})
+        Dim Orders = New List(Of Order)(New Order() {New Order With {.Key = 1,
+                                            .Order = "Order 1"}, New Order With {.Key = 2,
+                                            .Order = "Order 2"}, New Order With {.Key = 3,
+                                            .Order = "Order 3"}, New Order With {.Key = 4,
+                                            .Order = "Order 4"}, New Order With {.Key = 5,
+                                            .Order = "Order 5"}})
+        Dim query = From c In Customers Join o In Orders On c.Key Equals o.Key
+                    Select New With {Key c.Name, Key o.Order}
+        For Each q In query
+            ListBox1.Items.Add(q.Order.ToString() & "-->" & q.Name)
+        Next q
+
+    End Sub
+
+End Class
+Public Class Customer
+    Public Key As Integer
+    Public Name As String
+End Class
+Public Class Order
+    Public Key As Integer
+    Public Order As String
+End Class
+
